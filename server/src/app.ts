d0bd315bc88de56d213a278818cef364e8743db5
EmiPeralta <emilianop96@hotmail.com>
2020-12-07 16:14:21 -0300

    adding limit and extended true to urlencoded

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/api', postRoutes);
 
