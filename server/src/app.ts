7078ae6021dc322b77c9ce02162e8d7565004a32
EmiPeralta <emilianop96@hotmail.com>
2020-12-09 14:56:29 -0300

    some middlewares added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import compression from 'compression';
+import helmet from 'helmet';
 import morgan from 'morgan';
 
 import postRoutes from './routes/post.routes';
@@ -7,6 +9,8 @@ import postRoutes from './routes/post.routes';
 const app = express();
 
 app.use(cors());
+app.use(compression());
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
